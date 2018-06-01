bc11caa31976e5ac55a71423e88b8eff57083a69
Ashwin Hegde <ashwin.hegde12@gmail.com>
2018-06-01 08:22:50 +0530

    Hashdiff lib

---
+++ lib/paper_trail_hashdiff.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# Allows storing only incremental changes in the object_changes column
+# Uses HashDiff (https://github.com/liufengyun/hashdiff)
+class PaperTrailHashDiff
+  def diff(changes)
+    diff_changes = {}
+    changes.each do |field, value_changes|
+      diff_changes[field] = HashDiff.diff(value_changes[0], value_changes[1], array_path: true)
+    end
+    diff_changes
+  end
+
+  def where_object_changes(version_model_class, attributes)
+    scope = version_model_class
+    attributes.each do |k, v|
+      scope = scope.where("(((object_changes -> ?)::jsonb ->> 0)::jsonb @> ?)", k.to_s, v.to_s)
+    end
+    scope
+  end
+end
---
+++ paper_trail_hashdiff.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'paper_trail_hashdiff'
+  s.version     = '0.0.1'
+  s.date        = '2010-04-28'
+  s.summary     = "Paper Trail Hashdiff"
+  s.description = "Allows storing only incremental changes in the object_changes column"
+  s.authors     = ["Ashwin Hegde"]
+  s.email       = 'ashwin.hegde12@gmail.com'
+  s.files       = ["lib/paper_trail_hashdiff.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/paper_trail_hashdiff'
+  s.license       = 'MIT'
+end
